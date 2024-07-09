32e51b7619ec53c320ae640e7ef5534e2543576b
miruoon <rillakerika@gmail.com>
2024-07-09 19:51:06 +0900

    Initial Commit

---
+++ QiitaSwiftClient/QiitaSwiftClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  QiitaSwiftClientApp.swift
+//  QiitaSwiftClient
+//
+//  Created by erika yoshikawa on 2024/07/09.
+//
+
+import SwiftUI
+
+@main
+struct QiitaSwiftClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
